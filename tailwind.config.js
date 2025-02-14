cb1aa5f58e63587086d97ae9ad1524f4f2493cb4
Innocentprogrammer <mratyunjaysaxena8859@gmail.com>
2025-02-14 22:00:44 +0530

    calculate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",  // Add this to cover your custom directories
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
